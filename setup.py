3a7f6de49d27ca7d59c63281273a97c1bd8d701a
Martin Siggel <martin.siggel@dlr.de>
2019-03-10 21:04:09 +0100

    Fixed setup.py style and dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup
-from distutils.extension import Extension
-
 
 setup(
-	name='cpacs2to3',
-	version='0.3',
-	description='Converts CPACS 2 files to CPACS3 files',
-	author='Martin Siggel',
-	author_email=',martin.siggel@dlr.de',
-	license='Apache-2.0',
-	requires=['tigl3', 'tigl', 'tixi'],
-	packages=['cpacs2to3'],
-	entry_points={
-            'console_scripts': ['cpacs2to3 = cpacs2to3.cpacs_converter:main']}
+    name='cpacs2to3',
+    version='0.9',
+    description='Converts CPACS 2 files to CPACS3 files',
+    author='Martin Siggel',
+    author_email=',martin.siggel@dlr.de',
+    license='Apache-2.0',
+    requires=['tigl3', 'tigl', 'tixi', 'semver', 'numpy'],
+    packages=['cpacs2to3'],
+    entry_points={
+        'console_scripts': ['cpacs2to3 = cpacs2to3.cpacs_converter:main']}
 )
